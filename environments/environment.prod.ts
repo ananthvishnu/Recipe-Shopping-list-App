dc987dc4e1e61e1fbcdc8ccd62a27fb8572cc0b8
Ananthakumar Vishnurathan <124557188+ananthvishnu@users.noreply.github.com>
2023-08-19 15:42:30 +0530

    environment variable setup

---
+++ environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey:'AIzaSyCTNQbG-j7-XPybxzQGLHQR_dZ4ZyALKVY'
+};
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
